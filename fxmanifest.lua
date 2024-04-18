3d2d248cbbbab02f843613477aa7adb530c6d257
Strife Social <166969842+strifesocial@users.noreply.github.com>
2024-04-18 14:50:55 -0400

    Created - fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'rdr3'
+
+author 'Strife Social Developments'
+description 'Vehicle Spawning Script'
+version '1.0'
+
+client_script 'client.lua'
+server_script 'server.lua'
+
+lua54 'yes'
